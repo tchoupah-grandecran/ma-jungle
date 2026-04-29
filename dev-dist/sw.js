60a0c5f19cf69a8f9afc98cd4e12df10e7d8682c
Kevin <gqsmq58zng@privaterelay.appleid.com>
2026-04-29 16:16:22 +0200

    move font

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n1rg2s1k5g"
+    "revision": "0.fhj4kbi61ds"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

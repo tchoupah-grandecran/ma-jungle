40accaa613940cd35bbdcfc89c73d5e1e175e172
Kevin <gqsmq58zng@privaterelay.appleid.com>
2026-05-04 14:03:00 +0200

    fine tuning

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vmhjrcgjgsc"
+    "revision": "0.l1bock78ibg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
